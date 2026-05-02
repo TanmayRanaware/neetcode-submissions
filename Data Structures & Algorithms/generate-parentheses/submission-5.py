0052a092ea40f85f4b69e8ebb3134fd356ef5b13
rootTR <56064349+TanmayRanaware@users.noreply.github.com>
2026-05-01 19:15:01 -0700

    Add: generate-parentheses - submission-5

---
+++ Data Structures & Algorithms/generate-parentheses/submission-5.py
@@ -0,0 +1,55 @@
+class Solution:
+    # brute- 
+    # Total strings generated: 2^(2n) = 4^n
+    # Each validity check costs O(n)
+    # Time: O(n * 4^n)
+    # Space: O(n) recursion, excluding output
+    # def isValid(self, ch):
+    #     balance=0
+    #     for c in ch:
+    #         if c=='(':
+    #             balance+=1
+    #         else:
+    #              balance-=1 
+    #         if  balance<0:
+    #             return False
+    #     return balance==0          
+
+    # def combinations(self, res, ch, n):
+    #     if len(ch)==2*n:
+    #         if self.isValid(ch):
+    #             res.append(ch)
+    #         return     
+    #     self.combinations(res, ch+'(', n)
+    #     self.combinations(res, ch+')', n) 
+
+
+    # def generateParenthesis(self, n: int) -> List[str]:
+    #     res=[]
+    #     self.combinations(res, "", n)
+    #     return res
+
+    # optimal
+    #Time: O(Cn)
+    # More precisely with string building: O(n * Cn)
+    # Space: O(n) recursion stack, excluding output
+    # Where Cn is approximately: 4^n / (n^(3/2))  
+    # Time: O(n * Cn)
+    # Space: O(n) recursion stack, excluding output 
+    def backtrack(self, n, cur, res, open_count, closed_count):
+        if len(cur)==2*n:
+            res.append(cur)
+            return
+        if open_count < n :
+            self.backtrack(n, cur+"(", res, open_count+1, closed_count)
+        if closed_count < open_count:
+            self.backtrack(n, cur+")", res, open_count, closed_count+1)    
+
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        res=[]
+        self.backtrack(n, "", res, 0,0)
+        return res
+
+
+        
